ec1e76b30024f946bf6af967a1d59cdb27b1cd59
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-04-06 16:01:27 +0300

    Add enumerable module (any, all methods)

---
+++ my_enumerable.rb
@@ -0,0 +1,11 @@
+module MyEnumerable
+    def all?
+      each { |item| return false unless yield(item) }
+      true
+    end
+  
+    def any?
+      each { |item| return true if yield(item) }
+      false
+    end
+end  
